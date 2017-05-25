76d1bd607917d8e238b1c00d8ba1ef92c3dfef17
rodrigoshow <a14026@bcc.unifal-mg.edu.br>
2017-05-25 18:50:09 -0300

    Primeiro commit!

---
+++ calculadora.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int soma(int a, int b){
+	return a+b;
+}
+
+int subtrai(int a, int b){
+	return a-b;
+}
+
+int multiplica(int a, int b){
+	return a*b;
+}
+
+int divide(int a, int b){
+	
+	return a/b;
+}
+
+int main(){
+	printf("Hello git!");
+	printf("\n");
+	return 0;
+}
